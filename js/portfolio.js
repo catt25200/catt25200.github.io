560243df9d0565d60669f5526b2db9b92f0c693a
catt25200 <71999237+catt25200@users.noreply.github.com>
2022-02-18 16:11:41 +0800

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,22 @@
+(function() {
+    "use strict";
+    /**
+     * Initiate portfolio lightbox 
+     */
+     const portfolioLightbox = GLightbox({
+      selector: '.portfolio-lightbox'
+    });
+    
+  
+    /**
+     * Animation on scroll
+     */
+    window.addEventListener('load', () => {
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+      })
+    });
+  })()
